25cf0a573cc3ff5dcf20d535c6b992819cbcedb7
BimalPandeyCode <bimalpandey32@gmail.com>
2021-05-07 17:18:54 +0545

    4th day

--- src/CategoriesPage/IndividualCategoriesPage.js
+++ src/CategoriesPage/IndividualCategoriesPage.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useRef } from "react";
 import { BrowserRouter as Router, useParams } from "react-router-dom";
 import "./IndividualCategoriesPage.css";
 import data from "../data.js";
@@ -7,27 +7,92 @@ import Rating from "../Components/Rating/Rating.js";
 
 const IndividualCategoriesPage = () => {
   const { categoryId } = useParams();
-  const { name, id, image } = data[categoryId];
-  let output = [];
-  for (let i = 0; i < data.length; i++) {
-    output.push(<Product image={data[i].image} key={i} name={data[i].name} />);
-  }
-  return (
-    <React.Fragment>
-      <div className="IndividualCategoriesPage">
-        <div className="IndividualCategoriesPage-mainTitle">
-          <h2>{name}</h2>
-        </div>
-        <div className="IndividualCategoriesPage-sideSortBar">
-          <div style={{ position: "fixed" }}>
-            <div className="IndividualCategoriesPage-sideSortBar-priceSort"></div>
-            <div className="IndividualCategoriesPage-sideSortBar-ratingSort"></div>
+  let IndividualCategoriesPageSideSortBarRatingSort = useRef();
+  if (categoryId < data.length) {
+    const { name, id, image } = data[categoryId];
+    let output = [];
+    for (let i = 0; i < data.length; i++) {
+      output.push(
+        <Product image={data[i].image} key={i} name={data[i].name} />
+      );
+    }
+    return (
+      <React.Fragment>
+        <div className="IndividualCategoriesPage">
+          <div className="IndividualCategoriesPage-mainTitle">
+            <h3>{name}</h3>
+          </div>
+          <div className="IndividualCategoriesPage-sideSortBar">
+            <div className="IndividualCategoriesPage-sideSortBar-holder">
+              <div className="IndividualCategoriesPage-sideSortBar-priceSort">
+                <label>Sort by:</label>
+                <button>Popular</button>
+                {/* <button>UP</button> */}
+              </div>
+              <div className="IndividualCategoriesPage-sideSortBar-filter">
+                <i className="far fa-sort fa-2x">Sort</i>
+              </div>
+              <div
+                className="IndividualCategoriesPage-sideSortBar-ratingSort"
+                ref={IndividualCategoriesPageSideSortBarRatingSort}
+              >
+                <label>Rating:</label>
+                <button className="IndividualCategoriesPage-sideSortBar-ratingSort-button">
+                  <div className="IndividualCategoriesPage-sideSortBar-ratingSort-button-rating">
+                    <Rating n={"5"} />
+                  </div>
+                  <div className="IndividualCategoriesPage-sideSortBar-ratingSort-button-label">
+                    {/* <label>and Up</label> */}
+                  </div>
+                </button>
+                <button className="IndividualCategoriesPage-sideSortBar-ratingSort-button">
+                  <div className="IndividualCategoriesPage-sideSortBar-ratingSort-button-rating">
+                    <Rating n={"4"} />
+                  </div>
+                  <div className="IndividualCategoriesPage-sideSortBar-ratingSort-button-label">
+                    {/* <label>and Up</label> */}
+                  </div>
+                </button>
+                <button className="IndividualCategoriesPage-sideSortBar-ratingSort-button">
+                  <div className="IndividualCategoriesPage-sideSortBar-ratingSort-button-rating">
+                    <Rating n={"3"} />
+                  </div>
+                  <div className="IndividualCategoriesPage-sideSortBar-ratingSort-button-label">
+                    {/* <label>and Up</label> */}
+                  </div>
+                </button>
+                <button className="IndividualCategoriesPage-sideSortBar-ratingSort-button">
+                  <div className="IndividualCategoriesPage-sideSortBar-ratingSort-button-rating">
+                    <Rating n={"2"} />
+                  </div>
+                  <div className="IndividualCategoriesPage-sideSortBar-ratingSort-button-label">
+                    {/* <label>and Up</label> */}
+                  </div>
+                </button>
+                <button className="IndividualCategoriesPage-sideSortBar-ratingSort-button">
+                  <div className="IndividualCategoriesPage-sideSortBar-ratingSort-button-rating">
+                    <Rating n={"1"} />
+                  </div>
+                  <div className="IndividualCategoriesPage-sideSortBar-ratingSort-button-label">
+                    {/* <label>and Up</label> */}
+                  </div>
+                </button>
+              </div>
+            </div>
+          </div>
+          <div className="IndividualCategoriesPage-productsHolder">
+            {output}
           </div>
         </div>
-        <div className="IndividualCategoriesPage-productsHolder">{output}</div>
-      </div>
-    </React.Fragment>
-  );
+      </React.Fragment>
+    );
+  } else {
+    return (
+      <React.Fragment>
+        <h1>{categoryId}</h1>
+      </React.Fragment>
+    );
+  }
 };
 
 const Product = ({ image, key }) => {
