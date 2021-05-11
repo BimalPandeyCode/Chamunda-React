de049ace3ad6f9394e7ec7d191baa42c1264e275
BimalPandeyCode <bimalpandey32@gmail.com>
2021-05-11 19:25:19 +0545

    6th day

--- src/CategoriesPage/IndividualCategoriesPage.js
+++ src/CategoriesPage/IndividualCategoriesPage.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import { BrowserRouter as Router, useParams } from "react-router-dom";
 import "./IndividualCategoriesPage.css";
 import data from "../data.js";
@@ -6,18 +6,62 @@ import data from "../data.js";
 import Rating from "../Components/Rating/Rating.js";
 
 const IndividualCategoriesPage = () => {
-  const sortUp = <i className="fas fa-sort-up"></i>;
+  const [showSort, setShowSort] = useState(false);
+  const [sortName, setSortName] = useState(<span>Popular</span>);
+  function useOutsideAlerter(ref) {
+    useEffect(() => {
+      // Function for click event
+      function handleOutsideClick(event) {
+        if (ref.current && !ref.current.contains(event.target)) {
+          setShowSort(false);
+          // alert("you just clicked outside of box!");
+        }
+      }
+
+      // Adding click event listener
+      document.addEventListener("click", handleOutsideClick);
+    }, [ref]);
+  }
+  let sortClickEventListener = useRef(null);
+  useOutsideAlerter(sortClickEventListener);
   useEffect(() => {
     window.scrollTo(0, 0);
   }, []);
   const { categoryId } = useParams();
-  let IndividualCategoriesPageSideSortBarRatingSort = useRef();
+
+  const handleSetSortName = (input) => {
+    if (input === "popular") {
+      setSortName(<span className="fas">Popular</span>);
+      setShowSort(!showSort);
+    } else if (input === "priceUp") {
+      setSortName(
+        <span className="fas">Price&#xf161;</span>
+        // <i className="fas fa-sort-amount-down"></i>
+      );
+      setShowSort(!showSort);
+    } else if (input === "priceDown") {
+      setSortName(
+        <span className="fas">Price&#xf884;</span>
+        // <i className="fas fa-sort-amount-down"></i>
+      );
+      setShowSort(!showSort);
+    }
+  };
+
   if (categoryId < data.length) {
-    const { name, id, image } = data[categoryId];
+    const { name } = data[categoryId];
     let output = [];
     for (let i = 0; i < data.length; i++) {
       output.push(
-        <Product image={data[i].image} key={i} name={data[i].name} />
+        <Product
+          image={data[i].image}
+          key={i}
+          name={data[i].name}
+          rating={data[i].rating}
+          noOfRating={data[i].noOfRateing}
+          prevPrice={data[i].prevPrice}
+          currPrice={data[i].currPrice}
+        />
       );
     }
     return (
@@ -28,7 +72,45 @@ const IndividualCategoriesPage = () => {
           </div>
           <div className="IndividualCategoriesPage-sideSortBar">
             <div className="IndividualCategoriesPage-sideSortBar-holder">
-              <div className="IndividualCategoriesPage-sideSortBar-priceSort">
+              <div
+                className="PriceSortHolderButton-Div"
+                style={{ gridArea: "priceSort" }}
+                ref={sortClickEventListener}
+              >
+                <button
+                  className="fa PriceSortHolderButton"
+                  onClick={() => setShowSort(!showSort)}
+                >
+                  {sortName} {showSort ? <>&#xf0de;</> : <>&#xf0dd;</>}
+                </button>
+                {showSort ? (
+                  <React.Fragment>
+                    <div className="PriceSortHolder">
+                      <button
+                        className="PriceSortHolderButton"
+                        onClick={() => handleSetSortName("popular")}
+                      >
+                        Popular
+                      </button>
+                      <button
+                        className="PriceSortHolderButton"
+                        onClick={() => handleSetSortName("priceDown")}
+                      >
+                        Price<i className="fas fa-sort-amount-down-alt"></i>
+                      </button>
+                      <button
+                        className="PriceSortHolderButton"
+                        onClick={() => handleSetSortName("priceUp")}
+                      >
+                        Price<i className="fas fa-sort-amount-up"></i>
+                      </button>
+                    </div>
+                  </React.Fragment>
+                ) : (
+                  <></>
+                )}
+              </div>
+              {/* <div className="IndividualCategoriesPage-sideSortBar-priceSort">
                 <select className="fa">
                   <option value="0">Popular</option>
                   <option value="fas fa-sort-up">Price &#xf161;</option>
@@ -36,53 +118,35 @@ const IndividualCategoriesPage = () => {
                     Price &#xf884;
                   </option>
                 </select>
-              </div>
+              </div> */}
               <button className="IndividualCategoriesPage-sideSortBar-filter">
                 <i className="far fa-sort fa-1x">Sort</i>
               </button>
-              <div
-                className="IndividualCategoriesPage-sideSortBar-ratingSort"
-                ref={IndividualCategoriesPageSideSortBarRatingSort}
-              >
+              <div className="IndividualCategoriesPage-sideSortBar-ratingSort">
                 <label>Rating:</label>
                 <button className="IndividualCategoriesPage-sideSortBar-ratingSort-button">
                   <div className="IndividualCategoriesPage-sideSortBar-ratingSort-button-rating">
                     <Rating n={"5"} />
-                  </div>
-                  <div className="IndividualCategoriesPage-sideSortBar-ratingSort-button-label">
-                    {/* <label>and Up</label> */}
                   </div>
                 </button>
                 <button className="IndividualCategoriesPage-sideSortBar-ratingSort-button">
                   <div className="IndividualCategoriesPage-sideSortBar-ratingSort-button-rating">
                     <Rating n={"4"} />
                   </div>
-                  <div className="IndividualCategoriesPage-sideSortBar-ratingSort-button-label">
-                    {/* <label>and Up</label> */}
-                  </div>
                 </button>
                 <button className="IndividualCategoriesPage-sideSortBar-ratingSort-button">
                   <div className="IndividualCategoriesPage-sideSortBar-ratingSort-button-rating">
                     <Rating n={"3"} />
-                  </div>
-                  <div className="IndividualCategoriesPage-sideSortBar-ratingSort-button-label">
-                    {/* <label>and Up</label> */}
                   </div>
                 </button>
                 <button className="IndividualCategoriesPage-sideSortBar-ratingSort-button">
                   <div className="IndividualCategoriesPage-sideSortBar-ratingSort-button-rating">
                     <Rating n={"2"} />
                   </div>
-                  <div className="IndividualCategoriesPage-sideSortBar-ratingSort-button-label">
-                    {/* <label>and Up</label> */}
-                  </div>
                 </button>
                 <button className="IndividualCategoriesPage-sideSortBar-ratingSort-button">
                   <div className="IndividualCategoriesPage-sideSortBar-ratingSort-button-rating">
                     <Rating n={"1"} />
-                  </div>
-                  <div className="IndividualCategoriesPage-sideSortBar-ratingSort-button-label">
-                    {/* <label>and Up</label> */}
                   </div>
                 </button>
               </div>
@@ -103,7 +167,7 @@ const IndividualCategoriesPage = () => {
   }
 };
 
-const Product = ({ image, key }) => {
+const Product = ({ image, key, rating, noOfRating, prevPrice, currPrice }) => {
   return (
     <React.Fragment>
       <div className="IndividualCategoriesPage-product" key={key}>
@@ -114,17 +178,17 @@ const Product = ({ image, key }) => {
         />
         <div className="IndividualCategoriesPage-product-info">
           <p style={{ fontSize: "20px" }}>Product Name</p>
-          <Rating n={"2.5"} numberOfPeople={"133"} key={key} />
+          <Rating n={rating} numberOfPeople={noOfRating} key={key} />
           <p>
             Lorem Ipsum is a phrase that I do not understand, its probably
             something latin for how my cow stopped giving milk.
           </p>
           <div className="IndividualCategoriesPage-product-info-priceHolder">
             <p className="IndividualCategoriesPage-product-info-truePrice">
-              $1699.69
+              ${prevPrice}
             </p>
             <strike className="IndividualCategoriesPage-product-info-crossedPrice">
-              $1999.69
+              ${currPrice}
             </strike>
           </div>
           <div className="IndividualCategoriesPage-product-buttonHolder">
@@ -139,6 +203,30 @@ const Product = ({ image, key }) => {
             </button>
           </div>
         </div>
+      </div>
+    </React.Fragment>
+  );
+};
+
+const PriceSort = (setSortName) => {
+  return (
+    <React.Fragment>
+      <div className="PriceSortHolder">
+        <button
+          className="PriceSortHolderButton"
+          onClick={() => setSortName("Popular")}
+        >
+          Popular
+        </button>
+        <button
+          className="PriceSortHolderButton"
+          onClick={() => setSortName("Not so popular")}
+        >
+          Price<i className="fas fa-sort-amount-down"></i>
+        </button>
+        <button className="PriceSortHolderButton">
+          Price<i className="fas fa-sort-amount-up"></i>
+        </button>
       </div>
     </React.Fragment>
   );
--- src/data.js
+++ src/data.js
@@ -2,42 +2,70 @@ const data = [
   {
     id: 0,
     name: "Bimal Pandey",
+    rating: 0,
+    noOfRateing: 12,
+    prevPrice: 1000,
+    currPrice: 950,
     image:
       "https://images.unsplash.com/photo-1595835018349-198460e1d309?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=889&q=80",
   },
   {
     id: 1,
     name: "Shiva Pandey",
+    rating: 4.5,
+    noOfRateing: 25,
+    prevPrice: 900,
+    currPrice: 830,
     image:
       "https://images.unsplash.com/photo-1580757468214-c73f7062a5cb?ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8MTYlM0E5fGVufDB8fDB8fA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60",
   },
   {
     id: 2,
     name: "Binod Pandey",
+    rating: 3,
+    noOfRateing: 32,
+    prevPrice: 970,
+    currPrice: 954,
     image:
       "https://images.unsplash.com/photo-1547854760-f580559a870e?ixid=MnwxMjA3fDB8MHxzZWFyY2h8NHx8MTYlM0E5fGVufDB8fDB8fA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60",
   },
   {
     id: 3,
     name: "Purna Bahadur Pandey",
+    rating: 5,
+    noOfRateing: 100,
+    prevPrice: 950,
+    currPrice: 930,
     image:
       "https://images.unsplash.com/photo-1539601591461-2a5e0edb6915?ixid=MnwxMjA3fDB8MHxzZWFyY2h8N3x8MTYlM0E5fGVufDB8fDB8fA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60",
   },
   {
     id: 4,
-    name: "Gyanu Pandey",
+    name: "Dhal Kumari Pandey",
+    rating: 5,
+    noOfRateing: 120,
+    prevPrice: 1000,
+    currPrice: 1000,
     image:
       "https://images.unsplash.com/photo-1605804097616-ed12e891e514?ixid=MnwxMjA3fDB8MHxzZWFyY2h8OHx8MTYlM0E5fGVufDB8fDB8fA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60",
   },
   {
     id: 5,
     name: "Tejaswi Budathoki",
+    rating: 4,
+    noOfRateing: 1200,
+    prevPrice: 910,
+    currPrice: 870,
     image:
       "https://images.unsplash.com/photo-1525969173369-c9d513909d5d?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTR8fDE2JTNBOXxlbnwwfHwwfHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60",
   },
   {
     id: 6,
     name: "Hitesh Budathoki",
+    rating: 2.5,
+    noOfRateing: -1000,
+    prevPrice: 1000,
+    currPrice: 999,
     image:
       "https://images.unsplash.com/photo-1595835018349-198460e1d309?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=889&q=80",
   },
