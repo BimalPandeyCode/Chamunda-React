6fd7394383b13b61e2cd8a7f077c77f15b9fac8a
BimalPandeyCode <bimalpandey32@gmail.com>
2021-05-20 17:45:06 +0545

    13th day

--- src/data.js
+++ src/data.js
@@ -8,6 +8,11 @@ const data = [
     currPrice: 950,
     image:
       "https://images.unsplash.com/photo-1595835018349-198460e1d309?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=889&q=80",
+    otherImages: [
+      "https://images.unsplash.com/photo-1621390166430-2b004f6f4e39?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=401&q=80",
+      "https://images.unsplash.com/photo-1595835018349-198460e1d309?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=889&q=80",
+      "https://images.unsplash.com/photo-1621433117294-483ab26c0f24?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=755&q=80",
+    ],
   },
   {
     id: 1,
@@ -18,6 +23,11 @@ const data = [
     currPrice: 830,
     image:
       "https://images.unsplash.com/photo-1580757468214-c73f7062a5cb?ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8MTYlM0E5fGVufDB8fDB8fA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60",
+    otherImages: [
+      "https://images.unsplash.com/photo-1580757468214-c73f7062a5cb?ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8MTYlM0E5fGVufDB8fDB8fA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60",
+      "https://images.unsplash.com/photo-1595835018349-198460e1d309?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=889&q=80",
+      "https://images.unsplash.com/photo-1621433117294-483ab26c0f24?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=755&q=80",
+    ],
   },
   {
     id: 2,
@@ -28,6 +38,11 @@ const data = [
     currPrice: 954,
     image:
       "https://images.unsplash.com/photo-1547854760-f580559a870e?ixid=MnwxMjA3fDB8MHxzZWFyY2h8NHx8MTYlM0E5fGVufDB8fDB8fA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60",
+    otherImages: [
+      "https://images.unsplash.com/photo-1547854760-f580559a870e?ixid=MnwxMjA3fDB8MHxzZWFyY2h8NHx8MTYlM0E5fGVufDB8fDB8fA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60",
+      "https://images.unsplash.com/photo-1595835018349-198460e1d309?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=889&q=80",
+      "https://images.unsplash.com/photo-1621433117294-483ab26c0f24?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=755&q=80",
+    ],
   },
   {
     id: 3,
@@ -38,6 +53,11 @@ const data = [
     currPrice: 930,
     image:
       "https://images.unsplash.com/photo-1539601591461-2a5e0edb6915?ixid=MnwxMjA3fDB8MHxzZWFyY2h8N3x8MTYlM0E5fGVufDB8fDB8fA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60",
+    otherImages: [
+      "https://images.unsplash.com/photo-1539601591461-2a5e0edb6915?ixid=MnwxMjA3fDB8MHxzZWFyY2h8N3x8MTYlM0E5fGVufDB8fDB8fA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60",
+      "https://images.unsplash.com/photo-1595835018349-198460e1d309?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=889&q=80",
+      "https://images.unsplash.com/photo-1621433117294-483ab26c0f24?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=755&q=80",
+    ],
   },
   {
     id: 4,
@@ -48,6 +68,11 @@ const data = [
     currPrice: 1000,
     image:
       "https://images.unsplash.com/photo-1605804097616-ed12e891e514?ixid=MnwxMjA3fDB8MHxzZWFyY2h8OHx8MTYlM0E5fGVufDB8fDB8fA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60",
+    otherImages: [
+      "https://images.unsplash.com/photo-1605804097616-ed12e891e514?ixid=MnwxMjA3fDB8MHxzZWFyY2h8OHx8MTYlM0E5fGVufDB8fDB8fA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60",
+      "https://images.unsplash.com/photo-1595835018349-198460e1d309?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=889&q=80",
+      "https://images.unsplash.com/photo-1621433117294-483ab26c0f24?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=755&q=80",
+    ],
   },
   {
     id: 5,
@@ -58,6 +83,11 @@ const data = [
     currPrice: 870,
     image:
       "https://images.unsplash.com/photo-1525969173369-c9d513909d5d?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTR8fDE2JTNBOXxlbnwwfHwwfHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60",
+    otherImages: [
+      "https://images.unsplash.com/photo-1525969173369-c9d513909d5d?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTR8fDE2JTNBOXxlbnwwfHwwfHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60",
+      "https://images.unsplash.com/photo-1595835018349-198460e1d309?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=889&q=80",
+      "https://images.unsplash.com/photo-1621433117294-483ab26c0f24?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=755&q=80",
+    ],
   },
   {
     id: 6,
@@ -68,6 +98,11 @@ const data = [
     currPrice: 999,
     image:
       "https://images.unsplash.com/photo-1595835018349-198460e1d309?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=889&q=80",
+    otherImages: [
+      "https://images.unsplash.com/photo-1595835018349-198460e1d309?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=889&q=80",
+      "https://images.unsplash.com/photo-1595835018349-198460e1d309?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=889&q=80",
+      "https://images.unsplash.com/photo-1621433117294-483ab26c0f24?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=755&q=80",
+    ],
   },
 ];
 
--- src/product/product.js
+++ src/product/product.js
@@ -1,26 +1,76 @@
 import React from "react";
-import { BrowserRouter as Router, Link, useParams } from "react-router-dom";
+import { BrowserRouter as Router, useParams } from "react-router-dom";
+import Slider from "react-slick";
 import "./product.css";
 import data from "../data.js";
 
 const Product = () => {
   let { productID } = useParams();
+
+  const settings = {
+    customPaging: function (i) {
+      return (
+        <a href="##" className="productPageMainDiv-imageSlider-dots">
+          <img
+            className="productPageMainDiv-imageSlider-dots-image"
+            src={data[productID].otherImages[i]}
+            alt=""
+          />
+        </a>
+      );
+    },
+    dots: true,
+    dotsClass: "slick-dots slick-thumb",
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    arrows: false,
+  };
+
   return (
     <React.Fragment>
-      <img
-        src={data[productID].image}
-        alt=""
-        style={{ maxWidth: "50%", minWidth: "50%" }}
-      />
-      <h1>{data[productID].name}</h1>
-      <h1>${data[productID].currPrice}</h1>
-      <strike>
-        <h1>${data[productID].prevPrice}</h1>
-      </strike>
-      <button>Add to cart</button>
-      <br />
-      <br />
-      <button>BUY</button>
+      <div className="productPageMainDiv">
+        <Slider {...settings}>
+          <div>
+            <img
+              src={data[productID].otherImages[0]}
+              alt=""
+              className="productPageMainDiv-imageSlider-images"
+            />
+          </div>
+          <div>
+            <img
+              src={data[productID].otherImages[1]}
+              alt=""
+              className="productPageMainDiv-imageSlider-images"
+            />
+          </div>
+          <div>
+            <img
+              src={data[productID].otherImages[2]}
+              alt=""
+              className="productPageMainDiv-imageSlider-images"
+            />
+          </div>
+        </Slider>
+        <div className="productPageMainDiv-infoContainer">
+          <h1>{data[productID].name}</h1>
+          <h1>${data[productID].currPrice}</h1>
+          <strike>
+            <h1>${data[productID].prevPrice}</h1>
+          </strike>
+        </div>
+        <div className="productPageMainDiv-checkOutInfo">
+          <button>Add to cart</button>
+          <br />
+          <br />
+          <a href="https://ourworldindata.org/covid-vaccinations?country=NPL~OWID_WRL">
+            Covid19
+          </a>
+          <button>BUY</button>
+        </div>
+      </div>
     </React.Fragment>
   );
 };
